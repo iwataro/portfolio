1ca49f90c12f186fa33a5f98745951a60aeb1c97
iwataro <62655263+iwataro@users.noreply.github.com>
2021-06-02 21:42:45 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(function(){
+// AOS.js
+AOS.init({
+    duration: 1000,
+  });
+
+
+
+
+// スワイパー
+    var swiper = new Swiper('.swiper-container', {
+          // ここからはオプションです。
+        effect: 'coverflow',
+        centeredSlides: true,
+
+
+        autoplay: {
+            delay: 3000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false,
+            reverseDirection: false,
+        },
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        breakpoints: {
+            1: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            },
+            767: {
+                slidesPerView: 3,
+                spaceBetween: 0,
+            },
+        },
+        
+    });
+
+// スクロールヒント
+window.addEventListener('DOMContentLoaded', function(){
+    new ScrollHint('.js-scrollable');
+  });
+   
+  new ScrollHint('.js-scrollable', {
+    scrollHintIconAppendClass: 'scroll-hint-icon-white', // white-icon will appear
+    i18n: {
+      scrollable: 'スクロールできます'
+    }
+  });
+
+
+});
+
+
