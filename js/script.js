f6cde72c1bcff6b8ec83943aaa6b3e50e5526c05
iwataro <62655263+iwataro@users.noreply.github.com>
2021-06-02 23:06:28 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -43,6 +43,18 @@ window.addEventListener('DOMContentLoaded', function(){
   });
 
 
+  
+// 100vh修正
+  const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  
+  // 画面のサイズ変動があった時に高さを再計算する
+  window.addEventListener('resize', setFillHeight);
+  
+  // 初期化
+  setFillHeight();
 });
 
 
